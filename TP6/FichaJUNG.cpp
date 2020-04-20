31fbf62ea961044d15942b0de80e73a48d891b92
Rafael Cristino <rafavc2508@gmail.com>
2020-04-20 20:30:34 +0100

    [TP6] ex2 done

--- TP6/FichaJUNG.cpp
+++ TP6/FichaJUNG.cpp
@@ -39,7 +39,51 @@ void exercicio1()
 
 void exercicio2()
 {
+    GraphViewer * gv = new GraphViewer(600, 600, false);
+    gv->createWindow(600, 600);
 
+    gv->addNode(0, 300, 50);
+    gv->addNode(1, 318, 58);
+    gv->addNode(2, 325, 75);
+    gv->addNode(3, 318, 93);
+    gv->addNode(4, 300, 100);
+    gv->addNode(5, 282, 93);
+    gv->addNode(6, 275, 75);
+    gv->addNode(7, 282, 58);
+    gv->addNode(8, 150, 200);
+    gv->addNode(9, 300, 200);
+    gv->addNode(10, 450, 200);
+    gv->addNode(11, 300, 400);
+    gv->addNode(12, 200, 550);
+    gv->addNode(13, 400, 550);
+
+    gv->addEdge(0, 0, 1, EdgeType::DIRECTED);
+    gv->addEdge(1, 1, 2, EdgeType::DIRECTED);
+    gv->addEdge(2, 2, 3, EdgeType::DIRECTED);
+    gv->addEdge(3, 3, 4, EdgeType::DIRECTED);
+    gv->addEdge(4, 4, 5, EdgeType::DIRECTED);
+    gv->addEdge(5, 5, 6, EdgeType::DIRECTED);
+    gv->addEdge(6, 6, 7, EdgeType::DIRECTED);
+    gv->addEdge(7, 7, 0, EdgeType::DIRECTED);
+    gv->addEdge(8, 4, 9, EdgeType::DIRECTED);
+    gv->addEdge(9, 9, 8, EdgeType::DIRECTED);
+    gv->addEdge(10, 9, 10, EdgeType::DIRECTED);
+    gv->addEdge(11, 9, 11, EdgeType::DIRECTED);
+    gv->addEdge(12, 11, 12, EdgeType::DIRECTED);
+    gv->addEdge(13, 11, 13, EdgeType::DIRECTED);
+    gv->rearrange();
+
+    sleep(3);
+
+    gv->removeNode(12);
+    gv->removeNode(13);
+    gv->rearrange();
+
+    sleep(2);
+
+    gv->addNode(14, 250, 550);
+    gv->addNode(15, 350, 550);
+    gv->rearrange();
 }
 
 void exercicio3()
@@ -55,12 +99,12 @@ int main() {
     /*
      * Uncomment the line below to run Exercise 1
      */
-    exercicio1();
+    //exercicio1();
 
     /*
       * Uncomment the line below to run Exercise 2
       */
-    //exercicio2();
+    exercicio2();
 
     /*
       * Uncomment the line below to run Exercise 3
